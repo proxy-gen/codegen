138c4988f9cfe85d6ceebf1acb5ad7585b24b3f7
rvergis <rvergis@zynga.com>
2013-07-17 08:28:54 -0700

    remove facebook internal package

--- samples/platform/android/facebook/cxx/generated/project/FacebookCXX/src/FacebookCXX/com_facebook_internal_FileLruCache_StreamCloseCallback.java
+++
@@ -1,9 +0,0 @@
-/*
- * Implementation (Java)
- * Author: cxx-bindings-generator
- */
-package FacebookCXX
-public class com_facebook_internal_FileLruCache_StreamCloseCallback implements com.facebook.internal.FileLruCache.StreamCloseCallback
-{
-	public native void onClose();
-}
--- samples/platform/android/facebook/cxx/generated/project/FacebookCXX/src/FacebookCXX/com_facebook_internal_package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Implementation (Java)
- * Author: cxx-bindings-generator
- */
-package FacebookCXX
-public class com_facebook_internal_package-info implements com.facebook.internal.package-info
-{
-}
